daaf9650ede384ebe5d36efda34388e6c54c105c
hackerover90 <isaac_noori@outlook.com>
2023-12-09 18:04:22 -0500

    Center page content and set a maximum width

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,15 @@ import ResumeForm from './ResumeForm.jsx'
 import '../styles/App.css'
 
 function App() {
-  const [text, setText] = useState("a")
+  const [text, setText] = useState("")
   function handleChange(e) {
     setText(e.target.value)
   }
   return (
-    <>
+    <div id='app'>
       <ResumeForm value={text} onChange={handleChange} />
       <Resume text={text} />
-    </>
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function Header({ text }) {
     return (
-        <div className="header">
+        <div className="header ">
             <div id="headerName">{text}</div>
             <div id="headerInfo"></div>
         </div>
